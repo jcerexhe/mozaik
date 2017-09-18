a8791b93b857143a88dfa5abd87b6e7617de2884
Azzapop <aaron.b.hook@gmail.com>
2017-09-18 15:58:19 +1000

    react-helper

---
+++ client/index.js
@@ -0,0 +1,4 @@
+const reactHelper = require('react-helper');
+const search = require('../public/javascripts/components/search/index.jsx');
+
+reactHelper.register({ search });
--- webpack.config.js
+++ webpack.config.js
@@ -52,7 +52,7 @@ const config = {
   entry: {
     // we only have 1 entry, but I've set it up for multiple in the future
     App: './public/javascripts/mozaik-app.js',
-    Search: './public/javascripts/search.js',
+    react-helper: './client/index.js',
   },
   // we're using sourcemaps and here is where we specify which kind of sourcemap to use
   devtool: 'source-map',
