a0eec7bbb2e1a51eb116049de291b7b1930e98d7
Azzapop <aaron.b.hook@gmail.com>
2017-09-19 13:34:02 +1000

    update navbar so it doesnt scroll on menu open

--- public/javascripts/modules/menu.js
+++ public/javascripts/modules/menu.js
@@ -1,16 +1,18 @@
-export default window.onload = function() {
+export default window.onload = function () {
   const menu = document.getElementById('menu');
   const navbar = document.getElementById('nav');
   const isClear = navbar.classList.contains('clear');
-  document.getElementById('open-menu').onclick = function(e) {
+  document.getElementById('open-menu').onclick = function (e) {
     if (menu.classList.contains('hidden')) {
       menu.classList.remove('hidden');
       if (!isClear) navbar.classList.add('clear');
       this.classList.add('open');
+      document.body.classList.add('overflow-hidden');
     } else {
       menu.classList.add('hidden');
       if (!isClear) navbar.classList.remove('clear');
       this.classList.remove('open');
+      document.body.classList.remove('overflow-hidden');
     }
   }
 }
