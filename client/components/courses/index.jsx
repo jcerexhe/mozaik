dcc97a58ac1656766ad333cd3d21511a2b5dd7da
Jamie Cerexhe <jcerexhe@gmail.com>
2017-10-29 18:28:19 +1100

    school details redesign draft 1

--- client/components/courses/index.jsx
+++ client/components/courses/index.jsx
@@ -48,7 +48,7 @@ export default class CoursesApp extends Component {
     // Combine the 'all areas' array with the disciplineList array and flatten it to remove nesting
     disciplineList = _.flatten(disciplineList);
     return (
-      <div className='filter-bg course-bg'>
+      <div className='grey-bg course-bg'>
         <div className='filter-container'>
           <div className='filter'>
             <h2>disciplines</h2>
