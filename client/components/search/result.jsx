722559d9e72b61eafce2190c37da257bc8b48a90
Jessie Nerona <jbnerona@gmail.com>
2018-03-19 13:39:04 +0800

    search phase 2

--- client/components/search/result.jsx
+++ client/components/search/result.jsx
@@ -3,24 +3,31 @@ import likeCourse from '../shared/likeCourse';
 
 const Result = (props) => {
   const { result } = props;
-  let storageExists = localStorage.getItem('likedCourses')
-  let likedCourses;
-  if (storageExists !== null) {
-    likedCourses = JSON.parse(localStorage.getItem('likedCourses'));
-  } else {
-    likedCourses = []
-  }
-  let location = result.school.locations.map(location => {
-    return <div>{location.campus}, {location.country}</div>
-  })
+  // let storageExists = localStorage.getItem('likedCourses')
+  // let likedCourses;
+  // if (storageExists !== null) {
+  //   likedCourses = JSON.parse(localStorage.getItem('likedCourses'));
+  // } else {
+  //   likedCourses = []
+  // }
+  // let location = result.school.locations.map(location => {
+  //   return <div>{location.campus}, {location.country}</div>
+  // })
+  //  let location = result.locations.map(location => {
+  //   return <div>{location.campus}, {location.country}</div>
+  // })
   return (
-    <a className='search-result' href={ '/school/'+result.school.slug+'/'+result.slug+'/artwork' }>
-      <span className='course-name'>{ result.name }</span>
-      <span className='city'>{ location[0] }</span>
-      <img src={ result.image } alt={ result.name } />
-      <img className='logo' src={ result.school.logo } alt='school logo' />
-      <i onClick={() => likeCourse(result.name)} className={'fa ' + (likedCourses.includes(result.name) ? 'fa-heart ' : 'fa-heart-o ') + 'fa-2x'} aria-hidden="true"></i>
+    // <a className='search-result' href={ '/school/'+result.school.slug+'/'+result.slug+'/artwork' }>
+      // <span className='course-name'>{ result.name }</span>
+      // <span className='city'>{ location[0] }</span>
+      // <img src={ result.image } alt={ result.name } />
+      // <img className='logo' src={ result.school.logo } alt='school logo' />
+      // <i onClick={() => likeCourse(result.name)} className={'fa ' + (likedCourses.includes(result.name) ? 'fa-heart ' : 'fa-heart-o ') + 'fa-2x'} aria-hidden="true"></i>
+    // </a>
+    <a className='search-result' href={ '/school/'+result.slug }>
+      <span className='course-name'>{ result.slug }</span>
     </a>
+
   );
 }
 
--- controllers/apiController.js
+++ controllers/apiController.js
@@ -3,6 +3,7 @@ const cloudinary = require('cloudinary');
 const _ = require('lodash');
 
 const Course = mongoose.model('Course');
+const School = mongoose.model('School');
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
@@ -14,12 +15,15 @@ exports.courses = async (req, res) => {
   const limit = Number.parseInt(data.limit, 10);
   let q;
   let courses;
+  let schools;
+  let foundSchools;
   switch (data.searchCategory) {
     case 'discipline':
       const disc = JSON.parse(data.discipline);
       const areas = disc.interestAreas;
       const { interestDisciplines, interestCountries } = disc;
       const targets = [];
+      const schoollist = [];
       if (areas[0] !== 'all areas') {
         // areas is an array of selected areas
         // $all finds a document with all of the els in array
@@ -31,7 +35,8 @@ exports.courses = async (req, res) => {
 
       switch (targets.length) {
         case 0:
-          q = Course.find();
+          q = Course.find()
+          schoollist.push();
           break;
         case 1:
           q = Course.find(targets[0]);
@@ -42,6 +47,19 @@ exports.courses = async (req, res) => {
       const foundCourses = await q.populate('school', ['slug', 'locations', 'logo']);
       if (interestCountries.includes('all areas')) {
         courses = foundCourses;
+        courses.map(course => {
+          if(!schoollist.includes(course.school.slug)){
+            schoollist.push(course.school.slug);
+          }
+
+          schools = School.find({ slug: { $in: schoollist } });
+
+        });
+        foundSchools = await schools.populate('school');
+        // console.log(schoollist);
+        console.log(foundSchools.length);
+                console.log(foundSchools[0].slug);
+
       } else {
         courses = _.filter(foundCourses, (course) => {
           let locationsArray = course.school.locations.filter((location) => interestCountries.includes(location.country))
@@ -50,6 +68,18 @@ exports.courses = async (req, res) => {
           // if any locations were put into the array for this course then add the course to the returned courses array
           return false;
         });
+         courses.map(course => {
+          if(!schoollist.includes(course.school.slug)){
+            schoollist.push(course.school.slug);
+          }
+
+          schools = School.find({ slug: { $in: schoollist } });
+
+        });
+        foundSchools = await schools.populate('school');
+        // console.log(schoollist);
+        console.log(foundSchools.length);
+                console.log(foundSchools[0].slug);
       }
       break;
     case 'categories':
@@ -59,7 +89,7 @@ exports.courses = async (req, res) => {
       } else {
         q = Course.find();
       }
-      courses = await q.populate('school', ['slug', 'logo']);
+      courses = await q.populate('school', ['slug', 'locations', 'logo']);
       break;
     case 'search': {
       const regex = new RegExp(`.*${data.search}.*`, 'i');
@@ -74,14 +104,14 @@ exports.courses = async (req, res) => {
           { specialisations: { $in: split } },
         ],
       }).collation({locale: "en", strength: 2});
-      courses = await q.populate('school', ['slug', 'logo']);
+      courses = await q.populate('school', ['slug', 'locations', 'logo']);
       break;
     }
     default:
       q = Course.find();
   }
   // TODO allow user to change limit
-  res.json({results: courses.slice(0, limit), resultsAmount: courses.length});
+  res.json({results: foundSchools.slice(0, limit), resultsAmount: foundSchools.length});
   // q.populate('school', 'slug').limit(limit).then((courses) => {
     // res.json(courses);
   // }).catch(() => {
