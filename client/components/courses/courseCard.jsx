409be28fcdc4207da92708532e413379bb4557fd
Jessie Nerona <jbnerona@gmail.com>
2018-03-21 12:05:46 +0800

    course card alternate

--- client/components/courses/courseCard.jsx
+++ client/components/courses/courseCard.jsx
@@ -4,12 +4,6 @@ import likeCourse from '../shared/likeCourse';
 
 const CourseCard = (props) => {
   const { course, school, coursekey } = props;
-  const disciplines = ( course.disciplines.length > 0 ? (
-    <p className='course-info-bit-right'>
-      <span className='bold caps'>specialisations:</span>
-      <span className='info-bit-content'>{ course.specialisations.join(', ') }</span>
-    </p>
-  ) : <div />);
   // course liking func
   let storageExists = localStorage.getItem('likedCourses')
   let likedCourses;
@@ -19,15 +13,76 @@ const CourseCard = (props) => {
     likedCourses = []
   }
   let aligncourse;
+  let aligndesign;
+  let alignclass;
+
   if ((coursekey + 1)%2 == 0){
     aligncourse={left: '50px'};
+    alignclass='course-info-bit-left';
+    const disciplines = ( course.disciplines.length > 0 ? (
+    <p className={alignclass}>
+      <span className='bold caps'>specialisations:</span>
+      <span className='info-bit-content'>{ course.specialisations.join(', ') }</span>
+    </p>
+  ) : <div />);
+    return(
+      <div className='course-card' id={ course.slug } style={aligncourse}>
+        <div className="likesection">
+          <h3><a href={ '/school/'+school.slug+'/courses' }><i onClick={() => likeCourse(course.name)} className={'fa ' + (likedCourses.includes(course.name) ? 'fa-heart ' : 'fa-heart-o ') + 'card-icon'} aria-hidden="true" style={{float: 'left'}}></i></a></h3>
+        </div>
+        <div className='course-card-right'>
+          <div className='course-info'>
+            <div className='info-bits-container'>
+              <div className='info-bits-section'>
+                <p className={alignclass}>
+                  <span className='bold caps'>price</span>
+                  <span className='info-bit-content'>
+                    { _.map(course.prices, (price, i) => {
+                      return <span key={ i }>{ price.type }: AUD { price.fees }</span>;
+                    }) }
+                  </span>
+                </p>
+                { disciplines }
+                <p className={alignclass}>
+                  <span className='bold caps'>next intake</span>
+                  <span className='info-bit-content'>{ course.intakes }</span>
+                </p>
+              </div>
+            </div>
+            <div className='course-buttons' style={{textAlign: 'left'}}>
+              <a className='btn' href='#'>enquire</a>
+              <a className='btn' href='#'>apply</a>
+            </div>
+          </div>
+        </div>
+        <div className='course-card-right'>
+          <h3>{ course.name }</h3>
+          <div className='course-info'>
+            <p>{ course.description }</p>
+            <div className='info-bits-container'>
+              <div className='info-bits-section'>
+                <p className='course-info-bit-left'>
+                  <span className='bold caps'>length:</span>
+                  <span className='info-bit-content'>{ course.length }</span>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>);
   }else{
     aligncourse={left: '-50px'};
-  }
-  return (
-    <div className='course-card' id={ course.slug } style={aligncourse}>
+    alignclass='course-info-bit-right';
+        const disciplines = ( course.disciplines.length > 0 ? (
+    <p className={alignclass}>
+      <span className='bold caps'>specialisations:</span>
+      <span className='info-bit-content'>{ course.specialisations.join(', ') }</span>
+    </p>
+  ) : <div />);
+    return (
+      <div className='course-card' id={ course.slug } style={aligncourse}>
       <div className='course-card-right'>
-        <h3>{ course.name } <a href={ '/school/'+school.slug+'/courses' }><i onClick={() => likeCourse(course.name)} className={'fa ' + (likedCourses.includes(course.name) ? 'fa-heart ' : 'fa-heart-o ') + 'card-icon'} aria-hidden="true"></i></a></h3>
+        <h3>{ course.name }</h3>
         <div className='course-info'>
           <p>{ course.description }</p>
           <div className='info-bits-container'>
@@ -36,45 +91,15 @@ const CourseCard = (props) => {
                 <span className='bold caps'>length:</span>
                 <span className='info-bit-content'>{ course.length }</span>
               </p>
-              <p className='course-info-bit-left'>
-                <span className='bold caps'>price:</span>
-                <span className='info-bit-content'>
-                  { _.map(course.prices, (price, i) => {
-                    return <span key={ i }>{ price.type }: AUD { price.fees }</span>;
-                  }) }
-                </span>
-              </p>
-              { disciplines }
-            </div>
-            <div className='info-bits-section'>
-              <p className='course-info-bit-left'>
-                <span className='bold caps'>campus:</span>
-                <span className='info-bit-content'>
-                  <span>
-                  { _.map(course.campus, (campus, i) => {
-                    if(course.campus.length == (i+1)) {
-                      return `${ campus }`;  
-                    } else {
-                      return `${ campus } | `;
-                    }
-                  }) }
-                  </span>
-                </span>
-              </p>
-              <p className='course-info-bit-left'>
-                <span className='bold caps'>intakes:</span>
-                <span className='info-bit-content'>{ course.intakes }</span>
-              </p>
             </div>
           </div>
         </div>
       </div>
       <div className='course-card-right'>
-        <h3><a href={ '/school/'+school.slug+'/courses' }><i onClick={() => likeCourse(course.name)} className={'fa ' + (likedCourses.includes(course.name) ? 'fa-heart ' : 'fa-heart-o ') + 'card-icon'} aria-hidden="true"></i></a></h3>
         <div className='course-info'>
           <div className='info-bits-container'>
             <div className='info-bits-section'>
-              <p className='course-info-bit-right'>
+              <p className={alignclass}>
                 <span className='bold caps'>price</span>
                 <span className='info-bit-content'>
                   { _.map(course.prices, (price, i) => {
@@ -83,20 +108,24 @@ const CourseCard = (props) => {
                 </span>
               </p>
               { disciplines }
-              <p className='course-info-bit-right'>
-                <span className='bold caps'>intakes</span>
+              <p className={alignclass}>
+                <span className='bold caps'>next intake</span>
                 <span className='info-bit-content'>{ course.intakes }</span>
               </p>
             </div>
           </div>
           <div className='course-buttons'>
-            <a className='btn' href='#'>enquire {aligncourse.left} </a>
+            <a className='btn' href='#'>enquire</a>
             <a className='btn' href='#'>apply</a>
           </div>
         </div>
       </div>
+      <div className="likesection">
+        <h3><a href={ '/school/'+school.slug+'/courses' }><i onClick={() => likeCourse(course.name)} className={'fa ' + (likedCourses.includes(course.name) ? 'fa-heart ' : 'fa-heart-o ') + 'card-icon'} aria-hidden="true"></i></a></h3>
+      </div>
     </div>
-  );
+    );
+  }
 }
 
 export default CourseCard;
