7bd2d7fd5636737f96f519f5058f17b70b481328
grailwilly <grailjwi11y@gmail.com>
2018-03-22 12:26:36 +0800

    css layout

--- client/components/about/index.jsx
+++ client/components/about/index.jsx
@@ -5,25 +5,26 @@ export default class AboutApp extends Component {
 	render() {
 		return (
 		<div>
-		  <div className='static-about'>
-	        <div className='about-us'>
-	          <h3 className='hiddenline'>Hidden Line</h3>
-	          <p className='about-title'>About Us</p>
-	        </div>
-	      </div>
-	      <div className="about-image">
-	      	<img className="#" src="#"/>
-	      </div>
+			<div className='about-heading'>
+				<div className='about-info'>
+					<h1 className='hidden-line'> Using text to Hidden Line </h1>
+					<h1 className='about-title'> About us </h1>
+				</div>
+			</div>
 
-	      <div className="about-us-container">
-	      <p>hello</p>
-	      </div>
-	      
-	      <div className="part-footer">
-	      	<img src="/images/footer/black-lines.png"/>
-	      </div>
-	    </div> 
-	        	      
+			<div className='about-image'>
+				<p>img src="#" </p>
+			</div>
+			<div className='about-us-container'>
+				<p>content </p>
+			</div>
+			<div className='part-footer'>
+				<img src="/images/footer/black-lines.png" />
+			</div>
+
+
+		</div>
+        	      
 		);
 	}
 }
