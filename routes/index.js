49eaf35a068e9464d63bbeb7569155dc46bb107a
grailwilly <grailjwi11y@gmail.com>
2018-03-23 15:06:09 +0800

    partner display-flex

--- routes/index.js
+++ routes/index.js
@@ -24,4 +24,5 @@ router.get('/school/:schoolSlug/courses',
 );
 router.get('/discover', indexController.discover);
 router.get('/about-us',indexController.about);
+router.get('/partner-with-mozaik',indexController.partner);
 module.exports = router;
