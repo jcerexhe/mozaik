47659bbf44d7e16e4ab55899082d55c7904eb655
grailwilly <grailjwi11y@gmail.com>
2018-04-10 15:09:26 +0800

    footer-component

--- client/components/about/index.jsx
+++ client/components/about/index.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import _ from 'lodash';
-
+import Footer from '../footer/index.jsx';
 export default class AboutApp extends Component {
 	render() {
 		return (
@@ -29,6 +29,10 @@ export default class AboutApp extends Component {
 						<p>Say Hello</p>
 						<img src="/images/buttons/Contact-us.png" />
 					</div>
+					
+				</div>
+				<div>
+					<Footer/>
 				</div>
 			</div>
 			
