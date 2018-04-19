be5532fcb211934167186992dcf93b7f2cfa7430
grailwilly <grailjwi11y@gmail.com>
2018-04-19 08:11:51 +0800

    collapse css

--- client/components/agency/index.jsx
+++ client/components/agency/index.jsx
@@ -36,53 +36,25 @@ export default class AgencyApp extends Component {
 						</div>
 						
 					</div>
-					<div className='rectangle1'>
-					<div className="panel-group" id="accordion">
-  						<div className="panel panel-default">
-    						<div className="panel-heading">
-      <h4 className="panel-title">
-        <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">
-        Collapsible Group 1</a>
-      </h4>
-    </div>
-    <div id="collapse1" className="panel-collapse collapse in">
-      <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat.</div>
-    </div>
-  </div>
-  <div className="panel panel-default">
-    <div className="panel-heading">
-      <h4 className="panel-title">
-        <a data-toggle="collapse" data-parent="#accordion" href="#collapse2">
-        Collapsible Group 2</a>
-      </h4>
-    </div>
-    <div id="collapse2" className="panel-collapse collapse">
-      <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat.</div>
-    </div>
-  </div>
-  <div className="panel panel-default">
-    <div className="panel-heading">
-      <h4 className="panel-title">
-        <a data-toggle="collapse" data-parent="#accordion" href="#collapse3">
-        Collapsible Group 3</a>
-      </h4>
-    </div>
-    <div id="collapse3" className="panel-collapse collapse">
-      <div className="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat.</div>
-    </div>
-  </div>
-</div>
-			</div>
-			</div>
+					<div className='card'>
+						<div className='card-body'>
+							<div className="panel-group" id="accordion">
+		    					<div className="panel panel-default" id="panel1">
+		       						<div className="panel-heading">
+		             					<h4 className="panel-title">
+		        							<a data-toggle="collapse" data-target="#collapseOne" href="#collapseOne">
+		          								Collapsible Group Item #1
+		       								</a>
+		      							</h4>
+									</div>
+		        					<div id="collapseOne" className="panel-collapse collapse in">
+		            					<div className="panel-body">Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.</div>
+		        					</div>
+		    					</div>
+							</div>
+						</div>
+					</div>
+				</div>
 					{/*<div className='rectangle1'>
 						 <div className='discover'>
 						 	<div className='discover-title'>
