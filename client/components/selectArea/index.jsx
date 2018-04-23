b5b84ae1554acfd611b152c338e230e37331d629
grailwilly <grailjwi11y@gmail.com>
2018-04-23 09:34:01 +0800

    menu border

--- client/components/selectArea/index.jsx
+++ client/components/selectArea/index.jsx
@@ -20,41 +20,41 @@ export default class SelectAreaApp extends Component {
 				<div className="selected-study-areas">
 					<div className="selected-area-boxes">
 						<div className="selected-area card">
-	  							<img  src="http://res.cloudinary.com/mozaik/image/upload/v1522287645/study-areas/pexels-photo-433617.jpg" />
+	  							<img  src="http://res.cloudinary.com/mozaik/image/upload/v1522287645/study-areas/pexels-photo-433617.jpg" alt="Digital Media" />
 	    						<a href="/study-area/digital-media" className="btn btn-default">Digital Media</a>
 						</div>
 						<div className="selected-area card">
-	  							<img  src="https://res.cloudinary.com/mozaik/image/upload/v1522287309/study-areas/aleks-dorohovich-26-unsplash.jpg" />
+	  							<img  src="https://res.cloudinary.com/mozaik/image/upload/v1522287309/study-areas/aleks-dorohovich-26-unsplash.jpg" alt="Visual Communication" />
 	    						<a href="/study-area/visual-communication" className="btn btn-default">Visual Communication</a>
 						</div>
 						<div className="selected-area card">
-	  							<img  src="http://res.cloudinary.com/mozaik/image/upload/v1522287447/study-areas/pexels-photo-262034.jpg" />
+	  							<img  src="http://res.cloudinary.com/mozaik/image/upload/v1522287447/study-areas/pexels-photo-262034.jpg" alt="Fine Arts" />
 	    						<a href="/study-area/fine-arts" className="btn btn-default">Fine Arts</a>
 						</div>
 						<div className="selected-area card">
-	  							<img  src="http://res.cloudinary.com/mozaik/image/upload/v1522287352/study-areas/chris-murray-563843-unsplash.jpg" />
+	  							<img  src="http://res.cloudinary.com/mozaik/image/upload/v1522287352/study-areas/chris-murray-563843-unsplash.jpg" alt="FILM/TV/AUDIO" />
 	    						<a href="/study-area/film-tv-audio" className="btn btn-default">FILM/TV/AUDIO</a>
 						</div>
 					</div>
 					<div className="selected-area-boxes">
 						<div className="selected-area card">
-	  							<img  src="https://res.cloudinary.com/mozaik/image/upload/v1522806278/study-areas/pexels-photo-415307.jpg" />
+	  							<img  src="https://res.cloudinary.com/mozaik/image/upload/v1522806278/study-areas/pexels-photo-415307.jpg" alt="Performing Arts" />
 	    						<a href="/study-area/performing-arts" className="btn btn-default">Performing Arts</a>
 						</div>
 						<div className="selected-area card">
-	  							<img  src="http://res.cloudinary.com/mozaik/image/upload/v1522287247/study-areas/abstract-art-artistic-226589.jpg" />
+	  							<img  src="http://res.cloudinary.com/mozaik/image/upload/v1522287247/study-areas/abstract-art-artistic-226589.jpg" alt="Design"/>
 	    						<a href="/study-area/design" className="btn btn-default">Design</a>
 						</div>
 						<div className="selected-area card">
-	  							<img  src="http://res.cloudinary.com/mozaik/image/upload/v1522287581/study-areas/pexels-photo-298298.jpg" />
+	  							<img  src="http://res.cloudinary.com/mozaik/image/upload/v1522287581/study-areas/pexels-photo-298298.jpg" alt="Photography" />
 	    						<a href="/study-area/photography" className="btn btn-default">Photography</a>
 						</div>
 						<div className="selected-area card">
-	  							<img  src="http://res.cloudinary.com/mozaik/image/upload/v1522287362/study-areas/steve-driscoll-106346-unsplash.jpg" />
+	  							<img  src="http://res.cloudinary.com/mozaik/image/upload/v1522287362/study-areas/steve-driscoll-106346-unsplash.jpg" alt="Built Environment" />
 	    						<a href="/study-area/built-environment" className="btn btn-default">Built Environment</a>
 						</div>						
 						<div className="selected-area card">
-	  							<img  src="https://res.cloudinary.com/mozaik/image/upload/v1522287677/study-areas/pexels-photo-669615.jpg" alt="Card image cap" />
+	  							<img  src="https://res.cloudinary.com/mozaik/image/upload/v1522287677/study-areas/pexels-photo-669615.jpg" alt="Business for Creatives" />
 	    						<a href="/study-area/business-for-creatives" className="btn btn-default">Business for Creatives</a>
 						</div>						
 					</div>
