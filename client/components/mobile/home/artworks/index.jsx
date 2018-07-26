b645132922409f17310f5f288a1d5ec96076e524
marc-acm <marcmacariola1@gmail.com>
2018-07-26 11:08:07 +0800

    add artworks

---
+++ client/components/mobile/home/artworks/index.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import Modal from './modal.jsx';
+
+export default class MobileHomeArtworks extends Component {
+  constructor(props) {
+    super(props);
+
+    // Setup art indeces here so that it won't update everytime another state changes
+    let artIndeces = [];
+    for (var i=0; i<4; i++){
+      let artworks = this.props.artworks
+      let rdmArtIndex = Math.floor(Math.random() * Math.floor(artworks.length));
+      artIndeces.push(rdmArtIndex);
+    }
+
+    this.state = {
+      artIndeces: artIndeces,
+      artIndex: 0,
+      showArtModal: false
+    };
+  }
+
+  handleOpenArtModal (artIndex) {
+    this.setState({ showArtModal: true }, () => {
+      document.querySelector('.ReactModal__Body--open').style.overflow = "scroll" // Allows scrolling of main content even with modal open (needed because this is 'hidden' in desktop nav menu and mobile home hero video)
+    });
+  }
+
+  handleCloseArtModal () {
+    this.setState({ showArtModal: false });
+  }
+
+  // Returns artwork based on artIndex to the display
+  renderImg(artworks, artIndex) {
+    let artImgSrc = artworks[artIndex].images.src
+    return(
+      <div className='col'>
+        <div onClick={() => this.setState({artIndex: artIndex}, () => this.handleOpenArtModal())} style={{backgroundImage:'url(' + artImgSrc + ')'}}></div>
+      </div>
+    )
+  }
+
+  render() {
+    const { artworks } = this.props;
+    const { artIndeces, artIndex, showArtModal } = this.state;
+    return (
+      <div className='artworks-container' id="home-art-modal-parent"> { /* ID needed for modal config */ }
+        <div className='container-fluid'>
+          { /* Artworks display */ }
+          <div className='row'>
+            {this.renderImg(artworks, artIndeces[0])}
+            {this.renderImg(artworks, artIndeces[1])}
+          </div>
+          <div className='row'>
+            {this.renderImg(artworks, artIndeces[2])}
+            {this.renderImg(artworks, artIndeces[3])}
+          </div>          
+        </div>
+      </div>
+    );
+  }
+}
---
+++ client/components/mobile/home/artworks/modal.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import ReactModal from 'react-modal';
+// Config for react-modal
+ReactModal.setAppElement('body'); // To allow change of modal's parent
+// Custom styles for modal
+let modalStyles = {
+  overlay: {
+    backgroundColor: 'rgba(0, 0, 0, 0.4)',
+    position: 'absolute',
+  },
+  content: {
+    backgroundColor: '#ffffff',
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    height: '80%',
+    width: '55%',
+    transform: 'translate(-50%, -50%)',
+    border: 'none',
+    borderRadius: '0',
+    padding: '0',
+    boxShadow: '5px 5px 15px #000',
+    overflow: 'scroll'
+  }
+}
+
+export default class Modal extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  getArtModalParent() {
+    return document.querySelector('#home-art-modal-parent'); // In ./artworks.jsx
+  }
+
+  render() {
+    const { artworks, artIndex, showArtModal, handleCloseArtModal } = this.props; // See 'Modal' props in ./artworks.jsx
+    return (
+      <ReactModal
+        isOpen={showArtModal}
+        onRequestClose={handleCloseArtModal}
+        shouldCloseOnOverlayClick={true}
+        contentLabel="Art Modal"
+        parentSelector={() => this.getArtModalParent()}
+        style={modalStyles}
+      >
+        <div className="art-modal-image" style={{backgroundImage:'url(' + artworks[artIndex].images.src + ')'} /*Change style here since this varies*/}></div>
+        <div className="art-modal-caption">
+          <div className="caption-text">
+            <p>{artworks[artIndex].artist}</p>
+            <p>{artworks[artIndex].school_name}</p>
+          </div>
+          <button>VIEW MORE</button>
+        </div>
+      </ReactModal>
+    );
+  }
+}
