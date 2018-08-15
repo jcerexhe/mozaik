1719c7d3b325a79479e1d4de1ce5174f04740840
marc-acm <marcmacariola1@gmail.com>
2018-08-15 17:20:06 +0800

    areas and disciplines

--- client/components/mobile/search/discover.jsx
+++ client/components/mobile/search/discover.jsx
@@ -6,218 +6,123 @@ export default class MobileDiscover extends Component {
 	render() {
 		return (
 			<div className="mobile-discover-container">
-				<div className="header-discover-container">
-					<div className="container-fluid header-title">
-						DISCOVER
-					</div>
-				</div> 
-				<div className="searched-container">
-					<div className="container-fluid searched-section">
-						SEARCH...
-					</div>
-			    </div>
-				<div className="container-fluid study-area-section">
-					<div className="row">
-						<div className="col">
-				        	<div className="col-content">STUDY AREA</div>
-				        </div> 
-				        <div className="col">
-				        	<div className="col-content">STUDY AREA</div>
-				    	</div>
-				    	<div className="col">
-				      		<div className="col-content">STUDY AREA</div>
-				    	</div>
-					</div>
-				  	<div className="row">
-				    	<div className="col">
-				      		<div className="col-content">STUDY AREA</div>
-				    	</div> 
-				    	<div className="col">
-				      		<div className="col-content">STUDY AREA</div>
-				    	</div>
-				    	<div className="col">
-				      		<div className="col-content">STUDY AREA</div>
-				    	</div>
-					</div>			 	
-					<div className="row">
-				    	<div className="col">
-				      		<div className="col-content">STUDY AREA</div>
-				    	</div> 
-				    	<div className="col">
-				      		<div className="col-content">STUDY AREA</div>
-				    	</div>
-				    	<div className="col">
-				      		<div className="col-content">STUDY AREA</div>
-				    	</div>
-				   </div>
-				</div>
-				<div className="countries-container">
-					<div className="container-fluid country-section">
+				<div className="header"><h1>DISCOVER</h1></div>
+				
+				<form>
+					<input type="text" placeholder="SEARCH..." />
+			    </form>
+
+				<div className="study-areas-grid">
+					<div className="container-fluid">
 						<div className="row">
 							<div className="col">
-								<div className="col-country">AUSTRALIA</div>
-							</div>	
+					        	<button className="default-button">DIGITAL MEDIA</button>
+					        </div> 
+					        <div className="col">
+					        	<button className="default-button">VISUAL COMMS</button>
+					    	</div>
+					    	<div className="col">
+					      		<button className="default-button">FINE ARTS</button>
+					    	</div>
+						</div>
+					  	<div className="row">
+					    	<div className="col">
+					      		<button className="default-button">FILM/TV/AUDIO</button>
+					    	</div> 
+					    	<div className="col">
+					      		<button className="default-button">PERFORMING ARTS</button>
+					    	</div>
+					    	<div className="col">
+					      		<button className="default-button">DESIGN</button>
+					    	</div>
+						</div>			 	
+						<div className="row">
+					    	<div className="col">
+					      		<button className="default-button">PHOTOGRAPHY</button>
+					    	</div> 
+					    	<div className="col">
+					      		<button className="default-button">BUILD ENVIRONMENT</button>
+					    	</div>
+					    	<div className="col">
+					      		<button className="default-button">BUSINESS FOR CREATIVES</button>
+					    	</div>
+					   </div>
+					</div>   
+				</div>
+				<div className="countries-grid">
+					<div className="container-fluid">
+						<div className="row">
 							<div className="col">
-								<div className="col-country">CANADA</div>
+								<button className="default-button pink-btn country-button">AUSTRALIA</button>
 							</div>
 							<div className="col">
-								<div className="col-country">NEW ZEALAND</div>
+								<button className="default-button pink-btn country-button">CANADA</button>
 							</div>
 							<div className="col">
-								<div className="col-country">EUROPE</div>
+								<button className="default-button pink-btn country-button">NEW ZEALAND</button>
 							</div>
 							<div className="col">
-								<div className="col-country">USA</div>
+								<button className="default-button pink-btn country-button">EUROPE</button>
+							</div>
+							<div className="col">
+								<button className="default-button pink-btn country-button">USA</button>
 							</div>
 						</div>
 					</div>
 				</div> 
-				<div className="disciplines-container">
-					<div className="container-fluid grid-container">
-				   		<div className="row">
-				   	 		<div className="col">
-				        		<div className="col-content">STUDY AREA</div>
-				        	</div> 
-				        	<div className="col">
-				          		<div className="col-content">STUDY AREA</div>
-				        	</div>
-				        </div>	
-				   		<div className="row">
-				   	 		<div className="col">
-				          		<div className="col-content">STUDY AREA</div>
-				        	</div> 
-				        	<div className="col">
-				          		<div className="col-content">STUDY AREA</div>
-				        	</div>
-				   	 	</div>
-				   	 	<div className="row">
-				   			<div className="col">
-				          		<div className="col-content">STUDY AREA</div>
-				        	</div> 
-				       		<div className="col">
-				        		<div className="col-content">STUDY AREA</div>
-				 			</div> 
-				 		</div>	  
-				   	 	<div className="row">
-				   			<div className="col">
-				        		<div className="col-content">STUDY AREA</div>
-				        	</div>
-				        	<div className="col">
-				          		<div className="col-content">STUDY AREA</div>
-				        	</div>				        	
-				 		</div>	
-				   	 	<div className="row">
-				   			<div className="col">
-				          		<div className="col-content">STUDY AREA</div>
-				        	</div> 
-				        	<div className="col">
-				          		<div className="col-content">STUDY AREA</div>
-				 			</div>
-				 		</div>	
-				 	 	<div className="plus-button"></div>
-				 	</div>
-			 	 	<div className="clear-all-container">CLEAR FILTER</div>
-				</div>
-				<div className="sch-container">
-					<div className="container-fluid schs-container">
-				   		<div className="row">
-				   	 		<div className="col">
-				        		<div className="col-school">SCHOOL IMAGE</div>
-				        		<div className="sch-name">SCHOOL NAME</div>
-				        	</div> 
-				        	<div className="col">
-				          		<div className="col-school">SCHOOL IMAGE</div>
-				          		<div className="sch-name">SCHOOL NAME</div>
-				        	</div>
-				        	<div className="col">
-				        		<div className="col-school">SCHOOL IMAGE</div>
-				        		<div className="sch-name">SCHOOL NAME</div>
-				        	</div>
-				        </div>	
-				   		<div className="row">
-				   	 		<div className="col">
-				          		<div className="col-school">SCHOOL IMAGE</div>
-				          		<div className="sch-name">SCHOOL NAME</div>
-				        	</div> 
-				        	<div className="col">
-				          		<div className="col-school">SCHOOL IMAGE</div>
-				          		<div className="sch-name">SCHOOL NAME</div>
-				        	</div>
-				        	<div className="col">
-				          		<div className="col-school">SCHOOL IMAGE</div>
-				          		<div className="sch-name">SCHOOL NAME</div>
-				        	</div>
-				   	 	</div>
-				   	 	<div className="plus-button"></div>
-					</div> 					
-				</div>
-				<div className="adv-search-container">
-					<div className="container-fluid adv-search-title">
-						ADVANCED SEARCH
-					</div>
-				</div> 
-				<div className="quali-container">
-					<div className="container-fluid quali-section">
-						<div className="panel-group" id="accordion">
-  							<div className="panel panel-default">
-    							<div className="panel-heading">
-      								QUALIFICATIONS<a className="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseOne"></a>
- 								</div>
- 								<div id="collapseTwo" className="panel-collapse collapse">
- 								    <div className="panel-body">
-   			   						
-      								</div>
-    							</div> 
- 							</div>
- 						</div>
- 					</div>			
-				</div>
-				<div className="prange-container">
-					<div className="container-fluid prange-section">
-						PRICE RANGE	
-							<div className="ui-rangeslider-sliders">
-								<div className="ui-slider-track ui-shadow-inset ui-bar-inherit ui-corner-all">					   
-							    <div data-role="rangeslider">
-							    	<input type="range" name="price-min" id="price-min" value="200" min="0" max="1000"></input>
-							        <input type="range" name="price-max" id="price-max" value="800" min="0" max="1000"></input>
-							     </div>
-							    </div> 	
-							</div>		
-					</div>
-				</div>	
-				<div className="location-container">
-					<div className="container-fluid location-section">
-					 	<div className="panel-group" id="accordion">
-  							<div className="panel panel-default">
-    							<div className="panel-heading">
-      								LOCATION<a className="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseOne"></a>
- 								</div>
- 								<div id="collapseTwo" className="panel-collapse collapse">
- 								    <div className="panel-body">
-   			   						
-      								</div>
-    							</div> 
- 							</div>
- 						</div>
-					</div>
-				</div>
-				<div className="site-container">
-					<div className="container-fluid site-section">
-					 	<div className="row">	
-					 		<div className="col">
-					 			<p>ON CAMPUS</p>
+				<div className="disciplines-section">
+					<div className="grid">
+						<div className="container-fluid">	
+					   		<div className="row">
+					   	 		<div className="col">
+					        		<div className="col-content">2D/3D ANIMATION</div>
+					        	</div> 
+					        	<div className="col">
+					          		<div className="col-content">3D DESIGN</div>
+					        	</div>
+					        </div>	
+					   		<div className="row">
+					   	 		<div className="col">
+					          		<button className="default-button">AR/VR</button>
+					        	</div> 
+					        	<div className="col">
+					          		<button className="default-button">COMPUTER GRAPHICS</button>
+					        	</div>
+					   	 	</div>
+					   	 	<div className="row">
+					   			<div className="col">
+					          		<button className="default-button">DIGITAL DESIGN</button>
+					        	</div> 
+					       		<div className="col">
+					        		<button className="default-button">DIGITAL MEDIA</button>
+					 			</div> 
+					 		</div>	  
+					   	 	<div className="row">
+					   			<div className="col">
+					        		<button className="default-button">GAME DESIGN</button>
+					        	</div>
+					        	<div className="col">
+					          		<button className="default-button">MOTION GRAPHICS</button>
+					        	</div>				        	
+					 		</div>	
+					   	 	<div className="row">
+					   			<div className="col">
+					          		<button className="default-button">VISUAL EFFECTS (VFX)</button>
+					        	</div> 
+					        	<div className="col">
+					          		<button className="default-button">WEB DESIGN</button>
+					 			</div>
 					 		</div>
-					 		<div className="col">
-					 			<p>ONLINE</p>
-					 		</div>		
-						</div>					 
+					 	</div>		
+				 	 	<button className="default-button pink-btn plus">
+								<hr/>
+								<hr className="vertical"/>
+							</button>
+						</div>
+
+						<button className="default-button pink-btn clear-filter">CLEAR FILTER</button>
 					</div>
-				</div>
-				<div className="submit-container">
-					<div className="container-fluid submit-section">
-					 	SUBMIT
-					</div>
-				</div>
+								
 			</div>  
 		);
  	}
