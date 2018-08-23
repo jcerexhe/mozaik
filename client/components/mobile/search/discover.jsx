6090a8931c6a3c78a4818d54abab0ba20066f052
marc-acm <marcmacariola1@gmail.com>
2018-08-23 15:16:56 +0800

    adv search in discover

--- client/components/mobile/search/discover.jsx
+++ client/components/mobile/search/discover.jsx
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
 import Slider from 'rc-slider';
+import '!style-loader!css-loader!rc-slider/assets/index.css';
 import _ from 'lodash';
 
+const createSliderWithTooltip = Slider.createSliderWithTooltip;
+const Range = createSliderWithTooltip(Slider.Range)
+
 export default class MobileDiscover extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			priceRange: [0, 200000],
+			queries: {
+				search: '',
+				studyArea: [],
+				disciplines: [],	
+			},
+			results: []
+		}
+	}
+
+	updateSearchParam(key, val) {
+		this.setState({
+			[key]: val
+		})
+	}	 
 	render() {
+		const { priceRange } = this.state;
+
 		return (
 			<div className="mobile-discover-container">
 
@@ -169,6 +194,39 @@ export default class MobileDiscover extends Component {
 							<hr className="vertical"/>
 						</button>
 					</div>
+				</div>
+				<div className="adv-search-section">
+					<button className="default-button pink-btn adv-search-btn">ADVANCED SEARCH</button>
+					<div className="qpl">
+						<button className="default-button">QUALIFICATIONS<span><i className="fa fa-angle-down" aria-hidden="true"></i></span></button>
+						<div className="default-button price-range-btn">
+							PRICE RANGE
+							<div className="price-range-slider-container">
+								<Range
+									className="price-range-slider"
+									defaultValue={ [0, 200000] }
+									min={ 0 }
+									max={ 200000 }
+									value={ priceRange }
+									allowCross={ false }
+									onChange={ (val) => this.updateSearchParam('priceRange', val) }
+									marks={ { 0: <div className="center-text left-mark"><strong>$0</strong></div>, 200000: <div className="center-text right-mark"><strong>$200,000+</strong></div> } }
+								/>
+							</div>
+						</div>
+
+
+						<button className="default-button">LOCATION<span><i className="fa fa-angle-down" aria-hidden="true"></i></span></button>
+					</div>
+
+					<div className="location-btns">
+						<button>ON CAMPUS</button>
+						<button>ONLINE</button>
+					</div>
+
+					<button className="default-button pink-btn submit-btn">SUBMIT</button>
+
+
 				</div>
 								
 			</div>  
